4c1f32a098320e7ff80379372a6aefc1e04ce129
Phuong Thuan <phuonghiepthuan56@gmail.com>
2023-10-19 01:29:51 +0700

    setup

--- BE/Bird-Competition/src/main/java/com/birdcompetition/controller/DispatchServlet.java
+++ BE/Bird-Competition/src/main/java/com/birdcompetition/controller/DispatchServlet.java
@@ -1,8 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
-package com.birdcompetition;
+package com.birdcompetition.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -30,7 +26,7 @@ public class DispatchServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        String url = "postlogin.html";
+        String url = "index.html";
         try {
  
         }finally {
