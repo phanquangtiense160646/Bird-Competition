42de6f873a0ef463c556cad016a27be1f7121022
Phuong Thuan <phuonghiepthuan56@gmail.com>
2023-10-20 10:03:37 +0700

    set up

--- BE/Bird-Competition/src/main/java/com/birdcompetition/util/DBHelper.java
+++ BE/Bird-Competition/src/main/java/com/birdcompetition/util/DBHelper.java
@@ -13,11 +13,31 @@ import java.sql.SQLException;
  * @author Admin
  */
 public class DBHelper {
-    public static Connection getConnection() throws ClassNotFoundException, SQLException{
-        Connection conn= null;
+
+    public static Connection getConnection() 
+            throws ClassNotFoundException, SQLException {
+                //1. Get current context
+//        Context currentContext = new InitialContext();
+//        //2. Get web app server context
+//        Context tomcatContext = (Context)currentContext.lookup("java:comp/env");
+//        //3. Look up DS from web app context
+//        DataSource ds = (DataSource)tomcatContext.lookup("DSSE1729");
+//        //4. Get connection
+//        Connection con = ds.getConnection();
+//        
+//        return con;
+        
+        
+        //1. Load Driver
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        String url= "jdbc:sqlserver://localhost:1433;databaseName=BirdCompetionDB";
-        conn= DriverManager.getConnection(url, "sa", "12345");
-        return conn;
+        //2. Create connection String 
+        String url = "jdbc:sqlserver://" 
+                + "localhost:1433;"
+                + "databaseName=SE1729;"
+                +"encrypt=true;trustServerCertificate=true;";
+        //3. Open connection
+        Connection con = DriverManager.getConnection(url,"sa","12345");
+        //4. Return connection to caller
+        return con;
     }
 }
