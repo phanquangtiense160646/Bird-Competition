d36c9335acdcd47e9813c04f313cbfd48f9742dd
HiAmHyy <camusbatman147@gmail.com>
2023-10-20 08:18:06 +0700

    add login and ther

---
+++ BE/Bird-Competition/src/main/java/com/birdcompetition/util/Constants.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.birdcompetition.util;
+
+/**
+ *
+ * @author 84366
+ */
+public class Constants {
+    	public static String GOOGLE_CLIENT_ID = "507956590617-8srqvghri9v7r3r2joqe9ak8lnl0stdf.apps.googleusercontent.com";
+
+	public static String GOOGLE_CLIENT_SECRET = "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";
+
+	public static String GOOGLE_REDIRECT_URI = "http://localhost:8084/LoginFeature-3_1_2/login-google";
+
+	public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+
+	public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+
+	public static String GOOGLE_GRANT_TYPE = "authorization_code";
+}
