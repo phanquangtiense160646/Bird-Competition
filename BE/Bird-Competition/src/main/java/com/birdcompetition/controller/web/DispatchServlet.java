873485bc39da21aaf5ca7dfb627f2252a92c16ae
Phuong Thuan <phuonghiepthuan56@gmail.com>
2023-10-23 15:33:30 +0700

    fix schedule

--- BE/Bird-Competition/src/main/java/com/birdcompetition/controller/web/DispatchServlet.java
+++ BE/Bird-Competition/src/main/java/com/birdcompetition/controller/web/DispatchServlet.java
@@ -1,11 +1,6 @@
 package com.birdcompetition.controller.web;
 
-import com.birdcompetition.schedule.ScheduleDAO;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
