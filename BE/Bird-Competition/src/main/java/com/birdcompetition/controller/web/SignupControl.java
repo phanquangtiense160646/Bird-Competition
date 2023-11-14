b6aae0f22969b72602098a08adb26e805a87c5fe
Phuong Thuan <phuonghiepthuan56@gmail.com>
2023-11-14 22:58:53 +0700

    sua loi

--- BE/Bird-Competition/src/main/java/com/birdcompetition/controller/web/SignupControl.java
+++ BE/Bird-Competition/src/main/java/com/birdcompetition/controller/web/SignupControl.java
@@ -70,7 +70,7 @@ public class SignupControl extends HttpServlet {
                 //2. call DAO
                 DAO dao = new DAO();
 
-                User u = new User(user, re_pass);
+                User u = new User(user, re_pass, user, 0, user, url, user, url, pass, user, pass);
                 boolean result = dao.createAccount(u);
                 //3. Process Result
                 if (result) {
